567e90be143042b5878140f2018f2946ea6a8ff7
Amit Kumar <amkr.here@gmail.com>
2019-05-27 22:44:35 +0530

    Feature1 :facenet lib | basic ui (#4)
    * adding utility functions
    
    * adding facenet library (davidsandberg))
    
    * adding views functionality and apis
    
    * adding feature:1 | facenet lib
    
    * travis update
    
    * travis update 2
    
    * coverage to only tests
    
    * travis update for codefactor
    
    * travis update for codefactor
    
    * update readme
    
    * deleting unnecessary comment

--- cceface/models.py
+++ cceface/models.py
@@ -1,3 +1,10 @@
-# from django.db import models # NOQA
-
+from django.db import models
 # Create your models here.
+
+
+class InputVideo(models.Model):
+    video = models.FileField(upload_to='videos/')  # upload_to='videos', null=True, verbose_name=""
+
+
+class InputImage(models.Model):
+    image = models.FileField(upload_to='images/')  # upload_to='videos', null=True, verbose_name=""
