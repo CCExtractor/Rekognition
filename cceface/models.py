9b6318c95d447d7a946d3991eee4c388ff17d479
Amit <amkr.here@gmail.com>
2019-06-12 09:39:16 +0530

    updating models

--- cceface/models.py
+++ cceface/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 import uuid
 # Create your models here.
 
@@ -6,8 +7,7 @@ import uuid
 class InputVideo(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=80)
-    videofile = models.FileField(upload_to='videos/')  # upload_to='videos', null=True, verbose_name=""
-    created_on = models.DateField(auto_now_add=True)
+    created_on = models.DateTimeField(default=datetime.now, blank=True)
 
     def __str__(self):
         return self.id
@@ -16,8 +16,7 @@ class InputVideo(models.Model):
 class InputImage(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=80)
-    imagefile = models.FileField(upload_to='images/')  # upload_to='videos', null=True, verbose_name=""
-    created_on = models.DateField(auto_now_add=True)
+    created_on = models.DateTimeField(default=datetime.now, blank=True)
 
     def __str__(self):
         return self.id
