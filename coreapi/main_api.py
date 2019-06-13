5c6ccaf66e547dedbc6c14e3bb6a64568420fcaf
Amit <amkr.here@gmail.com>
2019-06-13 14:59:09 +0530

    patching small bugs  and adding caching functionality

--- coreapi/main_api.py
+++ coreapi/main_api.py
@@ -48,9 +48,8 @@ def FaceRecogniseInImage(request, filename):
     else:
         return {"Error": 'bad file format'}
 
-import time
+
 def FaceRecogniseInVideo(request, filename):
-    start_time = time.time()
     file_path = os.path.join(MEDIA_ROOT, 'videos/' + filename)
     handle_uploaded_file(request.FILES['file'], file_path)
     try:
@@ -71,7 +70,7 @@ def FaceRecogniseInVideo(request, filename):
     sim_cal = int(math.ceil(fps / 10))
     gap = (total_duration / total_frame) * sim_cal * 3 * 1000
 
-    print(' fps : ', fps, ' | tf : ', total_frame, ' | dur: ', total_duration, ' | frame_hop :', sim_cal, ' |  frame gap in ms : ', gap)
+    # print(' fps : ', fps, ' | tf : ', total_frame, ' | dur: ', total_duration, ' | frame_hop :', sim_cal, ' |  frame gap in ms : ', gap)
     count = 0
     cele = {}
     ids = []
@@ -83,13 +82,12 @@ def FaceRecogniseInVideo(request, filename):
         count = count + 1
         if count % sim_cal == 0:
             timestamps = (float(count) / fps) * 1000  # multiplying to get the timestamps in milliseconds
-            print(count)
             try:
                 all_faces, all_bb = get_face(img=curr_frame, pnet=pnet, rnet=rnet, onet=onet, image_size=image_size)
                 if all_faces is not None:
                     cele_id = []
-                    for img, bb in zip(all_faces, all_bb):
-                        embedding = embed_image(img=img, session=facenet_persistent_session, images_placeholder=images_placeholder, embeddings=embeddings,
+                    for face, bb in zip(all_faces, all_bb):
+                        embedding = embed_image(img=face, session=facenet_persistent_session, images_placeholder=images_placeholder, embeddings=embeddings,
                                                 phase_train_placeholder=phase_train_placeholder, image_size=image_size)
                         id_name = ''
                         if embedding_dict:
@@ -97,32 +95,23 @@ def FaceRecogniseInVideo(request, filename):
                                 id_name = identify_face(embedding=embedding, embedding_dict=cache_embeddings)
                                 if id_name is "Unknown":
                                     id_name = identify_face(embedding=embedding, embedding_dict=embedding_dict)
-                                    print("child embedding_dict")
                                     if id_name is not "Unknown":
                                         cache_embeddings[id_name] = embedding
-
-                                test = list(cache_embeddings.keys())
-                                print("cache_embeddings",test)
-
                             else:
                                 id_name = identify_face(embedding=embedding, embedding_dict=embedding_dict)
                                 cache_embeddings[id_name] = embedding
-                                print("parent embedding_dict")
 
                             if(str(id_name) not in ids):
                                 ids.append(str(id_name))
                                 cele[str(id_name)] = []
                             cele_id.append(id_name)
                             cele[str(id_name)].append(timestamps)
-
-
                 else:
                     return 'error no faces '
             except Exception as e:
                 return e
 
     output_dur = time_dura(cele, gap)
-    print("--- %s seconds ---" % (time.time() - start_time))
     return output_dur
 
 
