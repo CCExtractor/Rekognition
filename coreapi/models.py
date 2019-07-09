2764202c400a01c410c8fb9a9a04c7656d6cbf56
pymit <amkr.here@gmail.com>
2019-07-09 11:03:41 +0530

    Designing model for FeedBack feature

--- coreapi/models.py
+++ coreapi/models.py
@@ -25,10 +25,25 @@ class InputImage(models.Model):
 
 
 class InputEmbed(models.Model):
-    id = models.UUIDField(primary_key=True, editable=False)
+    id = models.CharField(primary_key=True, editable=False, max_length=50)
     title = models.CharField(max_length=80)
     fileurl = models.CharField(max_length=100, editable=False)
     created_on = models.DateTimeField(default=timezone.now, blank=True)
 
     def __str__(self):
         return self.id
+
+    def save(self, **kwargs):
+        if not self.id:
+            self.id = "{}".format(self.fileurl.split('/')[-1].split('.')[0])
+        super().save(*kwargs)
+
+
+class NameSuggested(models.Model):
+    suggestedName = models.CharField(max_length=80)
+    upvote = models.IntegerField(default=0)
+    downvote = models.IntegerField(default=0)
+    feedBack = models.ForeignKey(InputEmbed, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.suggestedName
