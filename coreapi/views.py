154f3d7351681bd259bf3794a000f76d54277046
pymit <amkr.here@gmail.com>
2019-07-11 12:27:57 +0530

    serializing multiple outputs

--- coreapi/views.py
+++ coreapi/views.py
@@ -77,10 +77,13 @@ class FeedbackFeature(APIView):
         embedList = InputEmbed.objects.all()
         randomFaceObject = embedList[random.randrange(len(embedList))]
         try:
-            nameSuggestedObject = NameSuggested.objects.get(feedback_id=randomFaceObject.id)
+            nameSuggestedObject = NameSuggested.objects.filter(feedback_id=randomFaceObject.id)
         except NameSuggested.DoesNotExist:
             nameSuggestedObject = NameSuggested.objects.create(suggestedName=randomFaceObject.title, feedback=randomFaceObject)
-        serializer = NameSuggestedSerializer([nameSuggestedObject, ], many=True)
+            nameSuggestedObject.save()
+
+        nameSuggestedList = NameSuggested.objects.filter(feedback_id=randomFaceObject.id)
+        serializer = NameSuggestedSerializer(nameSuggestedList, many=True)
         return Response({'data': serializer.data})
 
     def post(self, request, *args, **kwargs):
