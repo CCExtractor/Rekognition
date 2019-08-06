2a04810ca843c6ec297523147f4445d86b3edf67
pymit <amkr.here@gmail.com>
2019-08-06 12:41:54 +0530

    fix in REQUEST in embeddings

--- coreapi/urls.py
+++ coreapi/urls.py
@@ -5,8 +5,7 @@ from . import views
 urlpatterns = [
     path('image/', views.IMAGE_FR.as_view(), name='image_api'),
     path('old_video/', views.VIDEO_FR.as_view(), name='video_api'),
-    path('faceid/', views.LIST_AVAILABLE_EMBEDDING_DETAILS.as_view(), name='name_api'),
-    path('embednow/', views.CREATE_EMBEDDING.as_view(), name='embed_api'),
+    path('embed/', views.EMBEDDING.as_view(), name='embed_api'),
     path('video/', views.ASYNC_VIDEOFR.as_view(), name='celery_test_api'),
     path('feedback/', views.FeedbackFeature.as_view(), name='feedback_api'),
     path('nsfw/', views.NSFW_Recognise.as_view(), name='nsfw'),
