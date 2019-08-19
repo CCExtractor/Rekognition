892dd59bf9492164448fe6c9cb35522f61e8ce79
pymit <amkr.here@gmail.com>
2019-08-19 17:40:48 +0530

    adding shell script to download models for test

---
+++ download_model.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+pwd
+mkdir -p corelib/model/{facenet,tfs}
+wget https://www.dropbox.com/s/jm8grrifh5yk7is/2017.zip?dl=1  -O 2017.zip
+wget https://www.dropbox.com/s/zzjzdvx6523am20/fer2013.zip?dl=1 -O fer2013.zip
+unzip 2017.zip -d corelib/model/facenet/
+unzip fer2013.zip -d corelib/model/tfs/
