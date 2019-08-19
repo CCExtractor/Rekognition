108e6a4069c509528720cac8b11075bfd259f0b3
pymit <amkr.here@gmail.com>
2019-08-19 15:49:47 +0530

    fixing flake8 issues

--- coreapi/tests/test_models.py
+++ coreapi/tests/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from coreapi.models import *
+from coreapi.models import (InputVideo, InputImage)
 
 
 class InputVideoTest(TestCase):
@@ -11,7 +11,6 @@ class InputVideoTest(TestCase):
         return InputVideo.objects.create(title='VideoUnittest', id=1)
 
     def test_video_object(self):
-        video_object_id = self.create_video_object()
         expected_video_object = InputVideo.objects.get(id=1)
         expected_object_title = f'{expected_video_object.title}'
         self.assertEqual(expected_object_title, 'VideoUnittest')
@@ -26,11 +25,6 @@ class InputImageTest(TestCase):
         return InputImage.objects.create(title='ImageUnittest', id=2)
 
     def test_image_object(self):
-        image_object_id = self.create_image_object()
         expected_image_object = InputImage.objects.get(id=2)
         expected_object_title = f'{expected_image_object.title}'
         self.assertEqual(expected_object_title, 'ImageUnittest')
-
-
-if __name__ == "__main__":
-    main()
