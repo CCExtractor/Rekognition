ef5ffaa0779d2a743f611884f5f4fc11bfaa1e61
Pulkit Mishra <pulkitmishra007@gmail.com>
2020-04-09 02:53:43 +0530

    Adds Documrntation

--- coreapi/models.py
+++ coreapi/models.py
@@ -5,6 +5,15 @@ import uuid
 
 
 class InputVideo(models.Model):
+    """     Class to represent the input videos in the database
+
+    Workflow
+            *   id : primary key for input videos
+            *   title : name of input videos
+            *   isProcessesd : set true for processed videos and false for others
+            *   created_on : date and time of the input video
+
+    """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=80)
     isProcessed = models.BooleanField(default=False)
@@ -15,6 +24,15 @@ class InputVideo(models.Model):
 
 
 class InputImage(models.Model):
+    """     Class to represent the input images in the database
+
+    Workflow
+            *   id : primary key for input image
+            *   title : name of input image
+            *   isProcessesd : set true for processed image and false for others
+            *   created_on : date and time of the input image
+
+    """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=80,)
     isProcessed = models.BooleanField(default=False)
@@ -25,6 +43,15 @@ class InputImage(models.Model):
 
 
 class InputEmbed(models.Model):
+    """     Class to represent the embeddings in the database
+
+    Workflow
+            *   id : primary key for embeddings
+            *   title : name of embeddings file
+            *   fileurl : path of the saved embedding
+            *   created_on : date and time of the embeddings
+
+    """
     id = models.CharField(primary_key=True, editable=False, max_length=50)
     title = models.CharField(max_length=80)
     fileurl = models.CharField(max_length=100, editable=False)
@@ -40,6 +67,15 @@ class InputEmbed(models.Model):
 
 
 class NameSuggested(models.Model):
+    """     Class to represent the suggested name in the database for the feedback feature
+
+    Workflow
+            *   suggestedName : Suggested Name for the embedding
+            *   upvote : number of upvotes for the suggested name
+            *   downvote : number of downvotes for the suggested name
+            *   feedback : foreign key that connects NameSuggested to InputEmbed
+
+    """
     suggestedName = models.CharField(max_length=80)
     upvote = models.IntegerField(default=0)
     downvote = models.IntegerField(default=0)
@@ -50,6 +86,15 @@ class NameSuggested(models.Model):
 
 
 class SimilarFaceInImage(models.Model):
+    """     Class to represent the Similar Faces that have been found in image in the database
+
+    Workflow
+            *   id : primary key for image of the Similar Face
+            *   title : name of image of the Similar Face
+            *   similarwith : name of the image file which has the face that it is similar to 
+            *   created_on : date and time of the image of the Similar Face
+
+    """
     id = models.CharField(primary_key=True, editable=False, max_length=50)
     title = models.CharField(max_length=80)
     similarwith = models.CharField(max_length=80)
