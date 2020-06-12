f350f09e1367c2eb0ef606a374b2a182371ee17d
Pulkit Mishra <pulkitmishra007@gmail.com>
2020-06-12 22:39:39 +0530

    tests for Embeddings view

--- tests/test_views.py
+++ tests/test_views.py
@@ -27,8 +27,6 @@ class TestImageFr(TestCase):
 
         response1 = self.client.get('/api/image/')
         self.assertEqual(status.HTTP_200_OK, response1.status_code)
-        response2 = self.client.get('/api/image/')
-        self.assertEqual(status.HTTP_200_OK, response2.status_code)
 
 
 class TestNsfwRecognise(TestCase):
@@ -48,3 +46,27 @@ class TestNsfwRecognise(TestCase):
         self.assertEqual(status.HTTP_200_OK, response1.status_code)
         response2 = self.client.post('/api/nsfw/', {'file': self.uploaded_file2})
         self.assertEqual(status.HTTP_200_OK, response2.status_code)
+
+
+class TestEmbedding(TestCase):
+
+    def setUp(self):
+
+        super(TestEmbedding, self).setUp()
+        self.client = APIClient()
+        file1 = File(open('tests/testdata/t1.png', 'rb'))
+        self.uploaded_file1 = SimpleUploadedFile("temp1.png", file1.read(), content_type='multipart/form-data')
+        file2 = File(open('tests/testdata/t2.jpeg', 'rb'))
+        self.uploaded_file2 = SimpleUploadedFile("temp2.jpeg", file2.read(), content_type='multipart/form-data')
+
+    def test_post(self):
+
+        response1 = self.client.post('/api/embed/', {'file': self.uploaded_file1})
+        self.assertEqual(status.HTTP_200_OK, response1.status_code)
+        response2 = self.client.post('/api/embed/', {'file': self.uploaded_file2})
+        self.assertEqual(status.HTTP_200_OK, response2.status_code)
+
+    def test_get(self):
+
+        response1 = self.client.get('/api/embed/')
+        self.assertEqual(status.HTTP_200_OK, response1.status_code)
