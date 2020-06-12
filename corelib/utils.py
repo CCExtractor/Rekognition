465ecbacfea9c3a47e472b4f68510d0bf17f4bbe
Pulkit Mishra <pulkitmishra007@gmail.com>
2020-06-12 23:05:14 +0530

    style fix

--- corelib/utils.py
+++ corelib/utils.py
@@ -4,7 +4,7 @@ from enum import IntEnum
 class ImageFrNetworkChoices(IntEnum):
     MTCNN = 1
     RetinaFace = 2
-  
+
     @classmethod
     def choices(cls):
         return [(key.value, key.name) for key in cls]
