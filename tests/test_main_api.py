8520f89fc150c7c4c962bd86c06a183053147683
Bilal Ahmed <bilalahmedkarbelkar@gmail.com>
2021-03-23 07:36:15 +0530

    Added Unit Test for NSFW Classifier

--- tests/test_main_api.py
+++ tests/test_main_api.py
@@ -17,5 +17,19 @@ class TestObjectDetect(TestCase):
 		# result = json.loads(response.text)
 		self.assertEqual(result["Objects"][0][2]["Label"],self.label1)
 
-		
+class TestNsfwClassifier(TestCase):
+	def setUp(self):
+		super(TestNsfwClassifier,self).setUp()
+		self.url = "http://127.0.0.1:8000/api/nsfw/"
+		self.test_obj1 = {"file":open("./tests/testdata/nsfw.jpg",'rb')}
+		self.test_obj2 = {"file":open("./tests/testdata/drawing.jpg",'rb')}
+		self.label1 = "porn"
+		self.label2 = "neutral"
+
+	def test_nsfw_classification(self):
+		result1 = json.loads(requests.post(self.url, files=self.test_obj1).text)
+		self.assertEqual(result1["classes"].lower(),self.label1)
+
+		result2 = json.loads(requests.post(self.url, files=self.test_obj2).text)
+		self.assertEqual(result2["classes"].lower(),self.label2)
 
