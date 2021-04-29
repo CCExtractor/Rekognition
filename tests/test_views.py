8b7d01bebcd406c415a5b1db461943dcaa460405
Yash Bhambhu <43937889+YB221@users.noreply.github.com>
2021-04-30 04:32:03 +0530

    Update test_views.py

--- tests/test_views.py
+++ tests/test_views.py
@@ -4,7 +4,8 @@ from django.conf import settings
 from django.core.files.uploadedfile import SimpleUploadedFile
 from rest_framework import status
 settings.configure()
-from rest_framework.test import APIClient # noqa: E402
+from rest_framework.test import APIClient  # noqa: E402
+
 
 class TestImageFr(TestCase):
 
