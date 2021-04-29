4ba31a614c7086193918b966b8cc25531376bafb
Yash Bhambhu <43937889+YB221@users.noreply.github.com>
2021-04-30 04:55:42 +0530

    Update models.py

--- coreapi/models.py
+++ coreapi/models.py
@@ -2,11 +2,10 @@ from django.db import models
 from django.utils import timezone
 import uuid
 from django.conf import settings
-
-
 if not settings.configured:
     settings.configure()
 # Create your models here.
+
 class InputVideo(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=80)
